72b63ab058e288e233d2fced7071815680f095eb
Rodrigo Branas <rodrigo.branas@gmail.com>
2016-06-24 10:15:02 -0300

    generating dist

--- dist/angular-api.js
+++ dist/angular-api.js
@@ -22,21 +22,24 @@ var buildApi = function (config, http) {
 		if (options.resources) resource.resource = function (id) {
 			return createUrl(parent, resourceName, id) + "/";
 		};
-		resource.get = function (id, queryString) {
-			return http.get(addTimestamp(createUrl(parent, resourceName, id, queryString), queryString));
+		resource.get = function (id, queryString, config) {
+			return http.get(addTimestamp(createUrl(parent, resourceName, id, queryString), queryString), config);
 		};
-		resource.list = function (queryString) {
-			return http.get(addTimestamp(createUrl(parent, resourceName, undefined, queryString), queryString));
+		resource.list = function (queryString, config) {
+			return http.get(addTimestamp(createUrl(parent, resourceName, undefined, queryString), queryString), config);
 		};
-		resource.save = function (data, queryString) {
+		resource.save = function (data, queryString, config) {
 			if (options.beforeSave) data = options.beforeSave(data);
-			return http.post(createUrl(parent, resourceName, undefined, queryString), data);
+			return http.post(createUrl(parent, resourceName, undefined, queryString), data, config);
 		};
-		resource.update = function (id, data, queryString) {
-			return http.put(createUrl(parent, resourceName, id, queryString), data);
+		resource.update = function (id, data, queryString, config) {
+			return http.put(createUrl(parent, resourceName, id, queryString), data, config);
 		};
-		resource.delete = function (id, queryString) {
-			return http.delete(createUrl(parent, resourceName, id, queryString));
+		resource.patch = function (id, data, queryString, config) {
+			return http.patch(createUrl(parent, resourceName, id, queryString), data, config);
+		};
+		resource.delete = function (id, queryString, config) {
+			return http.delete(createUrl(parent, resourceName, id, queryString), config);
 		};
 		
 		resource.getOperations = function (id) {
@@ -44,9 +47,9 @@ var buildApi = function (config, http) {
 			var operations = {};
 			for(operation in options.operations) {
 				(function (operation) {
-					operations[operation] = function (data, queryString) {
+					operations[operation] = function (data, queryString, config) {
 						var method = options.operations[operation].method;
-						return http[method](addTimestamp(url + "/" + operation + ((queryString) ? createQueryString(queryString) : ""), ((queryString) ? createQueryString(queryString) : "")), data);
+						return http[method](addTimestamp(url + "/" + operation + ((queryString) ? createQueryString(queryString) : ""), ((queryString) ? createQueryString(queryString) : "")), data, config);
 					}
 				})(operation);
 			}
